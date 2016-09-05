576d4338fed4d8ccf9b0708f79f9bfd8be8947b8
Mark james <mjrun77@gmail.com>
2016-09-05 14:35:12 +0300

    Initial commit

---
+++ app/src/main/java/com/test/win/myapplication/fragments/FeedNewFragment.java
@@ -0,0 +1,31 @@
+package com.test.win.myapplication.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.test.win.myapplication.R;
+
+/**
+ * Created by Win on 05.09.2016.
+ */
+public class FeedNewFragment extends Fragment {
+    public FeedNewFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_feed_new, container, false);
+    }
+
+}
